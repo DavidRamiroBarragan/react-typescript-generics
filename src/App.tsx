a468be3f98c2514e30350d36a6afab0b199ac69a
M. David Ramiro <dramirobarragan@gmail.com>
2022-01-06 16:56:35 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import people from './mock-data/people';
+import widgets from './mock-data/widgets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Widgets</h2>
+      {widgets.map((w) => (
+        <h3>{w.title}</h3>
+      ))}
+      <h2>People</h2>
+      {people.map((p) => (
+        <h3>{`${p.firstName} ${p.lastName}`}</h3>
+      ))}
+    </>
   );
 }
 
